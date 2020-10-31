abea56b29e3fca57d8b6ef4e01fd046210b13675
stscoundrel <sampo@pixels.fi>
2020-10-31 17:33:57 +0200

    Add searchDictionary & test cases

---
+++ tests/lib/services/search.test.js
@@ -0,0 +1,48 @@
+import { getAllWords } from 'lib/services/dictionary'
+import { searchDictionary } from 'lib/services/search'
+
+describe('Search tests', () => {
+  const dictionary = getAllWords()
+
+  test('Returns array of results', () => {
+    const result = searchDictionary('skilja', dictionary)
+
+    expect(Array.isArray(result)).toBeTruthy()
+  })
+
+  test('Returns results in correct formatting', () => {
+    const result = searchDictionary('abbadís', dictionary)
+
+    const expected = {
+      word: 'abbadís',
+      definitions: ['f. <i>abbess.</i> Hkr. iii. 398, Fms. vii. 239, Gþl. 365.'],
+      slug: 'abbadis',
+    }
+
+    expect(result[0]).toMatchObject(expected)
+  })
+
+  test('Finds results from descriptions', () => {
+    const result = searchDictionary('f. <i>abbess.</i>', dictionary)
+
+    const expected = {
+      word: 'abbadís',
+      definitions: ['f. <i>abbess.</i> Hkr. iii. 398, Fms. vii. 239, Gþl. 365.'],
+      slug: 'abbadis',
+    }
+
+    expect(result[0]).toMatchObject(expected)
+  })
+
+  test('Finds results from slug', () => {
+    const result = searchDictionary('adal-vellir', dictionary)
+
+    const expected = {
+      word: 'aðal-vellir',
+      definitions: ['m. pl. = óðalvellir, Rm.'],
+      slug: 'adal-vellir',
+    }
+
+    expect(result[0]).toMatchObject(expected)
+  })
+})
