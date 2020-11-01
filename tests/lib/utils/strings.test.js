68a89ecc03d9f9cb3233e2618d4f10101cc58db9
stscoundrel <sampo@pixels.fi>
2020-11-01 18:58:54 +0200

    String utils: add removeHTML & testcase

--- tests/lib/utils/strings.test.js
+++ tests/lib/utils/strings.test.js
@@ -1,8 +1,13 @@
-import { capitalize } from 'lib/utils/strings'
+import { capitalize, removeHTML } from 'lib/utils/strings'
 
 describe('String utils tests', () => {
   test('Capitalizes text', () => {
     expect(capitalize('ja nyt kun mä meen')).toBe('Ja nyt kun mä meen')
     expect(capitalize('MITÄÄN KADU EN')).toBe('Mitään kadu en')
+  })
+
+  test('Removes html', () => {
+    expect(removeHTML('<strong>Strong</strong> <i>italics</i>')).toBe('Strong italics')
+    expect(removeHTML('<strong>Strong</strong> <strong>Strong</strong>')).toBe('Strong Strong')
   })
 })
