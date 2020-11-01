997db45b406123830d6cff3a1610913131b39fab
stscoundrel <sampo@pixels.fi>
2020-11-01 11:13:08 +0200

    Add Schema utils & test cases

---
+++ tests/lib/utils/schema.test.js
@@ -0,0 +1,74 @@
+import { getByLetter } from 'lib/services/dictionary'
+import { getSchema } from 'lib/utils/schema'
+
+describe('Schema structure tests', () => {
+  const dictionary = getByLetter('æ')
+  process.env.NEXT_PUBLIC_SITE_URL = 'https://cleasbyvigfusson.test'
+
+  test('Handles "word" Schema', () => {
+    const expected = JSON.stringify(
+      {
+        '@context': 'https://schema.org/',
+        '@type': 'DefinedTerm',
+        '@id': 'https://cleasbyvigfusson.test/word/aedi-vindr',
+        name: 'Cleasby & Vigfusson Dictionary - Æði-vindr',
+        description: 'm. <i>a furious gale,</i> Barl. 150.',
+        inDefinedTermSet: 'https://cleasbyvigfusson.test',
+      },
+    )
+
+    const result = getSchema(dictionary[15], 'word')
+
+    expect(result).toEqual(expected)
+  })
+
+  test('Handles "letter" Schema', () => {
+    const expected = JSON.stringify(
+      [
+        {
+          '@context': 'https://schema.org/',
+          '@type': 'DefinedTermSet',
+          '@id': 'https://cleasbyvigfusson.test/letter/ae',
+          name: 'Cleasby & Vigfusson Dictionary - Æ',
+          description: 'Old Norse words starting with letter Æ',
+        },
+        {
+          '@context': 'https://schema.org/',
+          '@type': 'DefinedTerm',
+          '@id': 'https://cleasbyvigfusson.test/word/aedi-vindr',
+          name: 'Cleasby & Vigfusson Dictionary - Æði-vindr',
+          description: 'm. <i>a furious gale,</i> Barl. 150.',
+          inDefinedTermSet: 'https://cleasbyvigfusson.test',
+        },
+        {
+          '@context': 'https://schema.org/',
+          '@type': 'DefinedTerm',
+          '@id': 'https://cleasbyvigfusson.test/word/aedr',
+          name: 'Cleasby & Vigfusson Dictionary - Æðr',
+          description: '<strong>1.</strong>',
+          inDefinedTermSet: 'https://cleasbyvigfusson.test',
+        },
+      ],
+    )
+
+    const result = getSchema(dictionary.slice(15, 17), 'letter')
+
+    expect(result).toEqual(expected)
+  })
+
+  test('Handles "default" Schema', () => {
+    const expected = JSON.stringify(
+      {
+        '@context': 'https://schema.org/',
+        '@type': 'DefinedTermSet',
+        '@id': 'https://cleasbyvigfusson.test',
+        name: 'Cleasby & Vigfusson Dictionary',
+        description: 'Old Norse words with English definitions',
+      },
+    )
+
+    const result = getSchema()
+
+    expect(result).toEqual(expected)
+  })
+})
