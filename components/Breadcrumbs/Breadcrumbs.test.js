d1e589e11689658b53aa40eb1909f7ec41f95c2c
stscoundrel <sampo@pixels.fi>
2020-12-18 21:15:25 +0200

    Add Breadcrumbs component & test cases

---
+++ components/Breadcrumbs/Breadcrumbs.test.js
@@ -0,0 +1,59 @@
+import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+import Breadcrumbs from './index'
+
+describe('Breadcrumbs component', () => {
+  test('Default page: does not crach', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Breadcrumbs type="page" />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  test('Default page: matches snapshot', () => {
+    const tree = renderer.create(<Breadcrumbs type="page" />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('Default page: renders correct amount of breadcrumbs', () => {
+    const tree = renderer.create(<Breadcrumbs type="page" />)
+    const { root } = tree
+
+    expect(root.findAllByType('a').length).toEqual(1)
+  })
+
+  test('Letter page: does not crach', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Breadcrumbs type="letter" content={[{ word: 'random' }]} />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  test('Letter page: matches snapshot', () => {
+    const tree = renderer.create(<Breadcrumbs type="letter" content={[{ word: 'random' }]} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('Letter page: renders correct amount of breadcrumbs', () => {
+    const tree = renderer.create(<Breadcrumbs type="letter" content={[{ word: 'random' }]} />)
+    const { root } = tree
+
+    expect(root.findAllByType('a').length).toEqual(2)
+  })
+
+  test('Word page: does not crach', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Breadcrumbs type="word" content={{ word: 'random' }} />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  test('Word page: matches snapshot', () => {
+    const tree = renderer.create(<Breadcrumbs type="word" content={{ word: 'random' }} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('Word page: renders correct amount of breadcrumbs', () => {
+    const tree = renderer.create(<Breadcrumbs type="word" content={{ word: 'random' }} />)
+    const { root } = tree
+
+    expect(root.findAllByType('a').length).toEqual(3)
+  })
+})
---
+++ components/Breadcrumbs/index.js
@@ -0,0 +1,39 @@
+import { getBreadcrumbs } from 'lib/utils/breadcrumbs'
+import { getSchema } from 'lib/utils/schema'
+import Link from 'next/link'
+
+export default function Breadcrumbs({ type, content }) {
+  const getBreadCrumbData = () => {
+    let letter = null
+    let word = null
+
+    if (type !== 'page') {
+      letter = type === 'letter' ? content[0].word.charAt(0) : content.word.charAt(0)
+    }
+
+    if (type === 'word') {
+      word = content.word
+    }
+
+    return {
+      type,
+      letter,
+      word,
+    }
+  }
+
+  const breadcrumbs = getBreadcrumbs(getBreadCrumbData())
+  const schema = getSchema(breadcrumbs, 'breadcrumbs')
+
+  return (
+    <div>
+      {breadcrumbs.map(({ label, url }) => (
+        <Link key={url} href={url}>
+          <a>{label}</a>
+        </Link>
+      ))}
+
+      <script type='application/ld+json' dangerouslySetInnerHTML={ { __html: schema } }/>
+    </div>
+  )
+}
