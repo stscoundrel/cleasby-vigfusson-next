627190870b10565840678e332120aed7c32a6b50
stscoundrel <sampo@pixels.fi>
2021-02-21 11:10:11 +0200

    Search results: add result count
    Fixes #25

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -3,12 +3,15 @@ import styles from './SearchResults.module.scss'
 
 export default function SearchResults({ words }) {
   return (
-    <ul className={styles.list}>
-      { words.map((word) => (
-        <li key={word.slug}>
-          <SearchTeaser data={word} />
-        </li>
-      )) }
-    </ul>
+    <>
+      <p className="blue">{words.length} results found</p>
+      <ul className={styles.list}>
+        { words.map((word) => (
+          <li key={word.slug}>
+            <SearchTeaser data={word} />
+          </li>
+        )) }
+      </ul>
+    </>
   )
 }
