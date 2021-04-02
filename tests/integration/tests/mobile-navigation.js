de8150dfa198469e698b3360b8c5dc6b0bc02909
stscoundrel <sampo@pixels.fi>
2021-04-02 14:03:43 +0300

    Integration tests: break navigation tests into two
    Seemed to have different reasons to fail, so better to treat them as separate things

--- tests/integration/tests/mobile-navigation.js
+++ tests/integration/tests/mobile-navigation.js
@@ -1,18 +1,3 @@
-it('Desktop navigation works', () => {
-  // Ensure screen is large enough for desktop menu.
-  cy.viewport('macbook-15')
-
-  cy.visit('/')
-
-  cy.get('a[href="/letter/a"]:visible').first().click()
-  // Go to a letter page from nav link.
-  cy.location('pathname').should('equal', '/letter/a')
-
-  // Navigate to another letter
-  cy.get('a[href="/letter/b"]:visible').first().click()
-  cy.location('pathname').should('equal', '/letter/b')
-})
-
 it('Mobile navigation works', () => {
   cy.viewport('iphone-6')
   cy.visit('/')
