478a1fb094207cebeda57c5813784303b7e2b7ad
stscoundrel <sampo@pixels.fi>
2021-04-03 12:46:19 +0300

    Word definition: show possible runic translation

--- src/components/WordDefinition/index.js
+++ src/components/WordDefinition/index.js
@@ -1,4 +1,5 @@
 import { capitalize } from 'lib/utils/strings'
+import { lettersToRunes } from 'younger-futhark'
 import styles from './WordDefinition.module.scss'
 
 export default function WordDefinition({ data }) {
@@ -8,6 +9,7 @@ export default function WordDefinition({ data }) {
     <article className={styles.section}>
       <header>
         <h1 className="capitalize" lang="non">{capitalize(word)}</h1>
+        <p>Possible runic inscription in <em>Younger Futhark:</em> { lettersToRunes(word) }</p>
         <p>Meaning of Old Norse word <em>&quot;{word}&quot;</em></p>
       </header>
 
