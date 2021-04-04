33b4da758a1979c2e42b37618943f4aef7c17401
stscoundrel <sampo@pixels.fi>
2021-04-04 17:57:06 +0300

    Add next-pwa & common conf

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-module.exports = {
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV !== 'production',
+  },
   sassOptions: {
     prependData: `
         @import 'styles/variables';
       `,
   },
-}
+})
