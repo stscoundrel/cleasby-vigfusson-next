99d0fe36d3edfcd42beb9b3137e4eb563851df6a
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-03-05 17:01:50 +0200

    Switch Babel to SWC. Drop Stryker.
    Closes #275

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,12 @@ module.exports = {
   ],
   env: {
     node: true,
-    es6: true,
+    es2020: true,
     jest: true,
     'cypress/globals': true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
   },
   globals: {
     document: true,
@@ -20,7 +23,6 @@ module.exports = {
     'jest',
     'cypress',
   ],
-  parser: '@babel/eslint-parser',
   settings: {
     react: {
       version: '17.0.1',
