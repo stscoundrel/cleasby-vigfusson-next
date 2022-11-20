add3a21438999059e53f52e405942c70f1a1178b
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-11-20 17:04:27 +0200

    Add global test mock for next/router

--- setupTests.ts
+++ setupTests.ts
@@ -5,4 +5,15 @@ process.env.NEXT_PUBLIC_SITE_URL = undefined;
 jest.useFakeTimers();
 jest.setSystemTime(new Date(2020, 1, 1));
 
+/**
+ * Mock router
+ */
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      asPath: '/test',
+    }
+  },
+}))
+
 export {};
