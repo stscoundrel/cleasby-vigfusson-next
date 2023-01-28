2f17dd9fcc6f642dcd543fb8e79e8384da7cb570
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2023-01-28 08:55:06 +0200

    Add older headword spelling variants
    Closes #442

--- src/lib/utils/strings.ts
+++ src/lib/utils/strings.ts
@@ -17,7 +17,27 @@ export const removeHTML = (content: string): string => {
   return filteredString
 }
 
-export default {
-  capitalize,
-  removeHTML,
-}
+const transformWithMap = (content: string, dictionary: Map<string, string>): string => {
+  let result = '';
+  const parts: string[] = content.split('');
+
+  parts.forEach((part) => {
+    const partKey = part.toLocaleLowerCase();
+
+    if (dictionary.has(partKey)) {
+      result += dictionary.get(partKey);
+    } else {
+      result += part;
+    }
+  });
+
+  return result;
+};
+
+export const getOlderSpelling = (headword: string): string => {
+  const NEW_TO_OLD = new Map([
+    ['ö', 'ǫ'],
+  ])
+
+  return transformWithMap(headword, NEW_TO_OLD);
+};
